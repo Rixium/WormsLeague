d12a09b8e39446e1294f5819058bce72708ec66d
David Eadie <david@eadie.net>
2020-05-06 16:21:35 +0100

    Add start of login method
    Still need to store the tokens and write code for refreshing when access token expires

--- CommandLine/src/Container/CliModule.cs
+++ CommandLine/src/Container/CliModule.cs
@@ -11,6 +11,7 @@ using Worms.Resources.Replays.Text;
 using Worms.Resources.Schemes;
 using Worms.Resources.Schemes.Binary;
 using Worms.Resources.Schemes.Text;
+using Worms.Server.Auth;
 using Worms.Slack;
 using Worms.WormsArmageddon.Replays;
 
@@ -55,6 +56,9 @@ namespace Worms.Container
             builder.RegisterType<ReplayLogGenerator>().As<IReplayLogGenerator>();
 
             builder.RegisterGeneric(typeof(ResourceGetter<>)).As(typeof(ResourceGetter<>));
+
+            // Login
+            builder.RegisterType<LoginService>().As<ILoginService>();
         }
 
         private static void RegisterOsModules(ContainerBuilder builder)
